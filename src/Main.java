2cf0ea643e93f82855224fbdb11fce96f3b29982
zhv <zhslma@gmail.com>
2017-12-01 21:06:08 +0400

    * Stock interface * ReplaceOrder handling demonstrate implementation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import Car.AbstractCar;
+import Detail.AbstractDetail;
+import Orders.CompletedOrder;
+import Orders.ReplaceOrder;
+
 public class Main {
 
+    /*
+     * Order -> Station
+     * Order contains list of details
+     * Station check list of details for availability in Stock, Stock return details
+     *
+     * Stock removes returned details from internal list
+     *
+     *
+     */
     public static void main(String[] args) {
 	// write your code here
     }
+
+
+    Stock stock;
+
+    public CompletedOrder doWork(ReplaceOrder ro) {
+        AbstractCar car = ro.getCar();
+        String detailName = ro.getDetailName();
+
+        AbstractDetail oldDetail = car.removeDetail(detailName);
+        AbstractDetail newDetail = stock.getDetail(detailName);
+        car.addDetail(newDetail);
+
+        long balance = 0;
+        balance += oldDetail.getDetailPrice();
+        balance -= newDetail.getDetailPrice();
+
+        return new CompletedOrder(car, balance);
+    }
+
 }
---
+++ src/Orders/CompletedOrder.java
@@ -0,0 +1,33 @@
+package Orders;
+
+import Car.AbstractCar;
+
+/**
+ * @author VYZH
+ * @since 30.11.2017
+ */
+public class CompletedOrder extends AbstractOrder {
+
+    private AbstractCar car;
+    private long price;
+
+    public CompletedOrder(AbstractCar priceForCar, long id) {
+        super(priceForCar, id);
+    }
+
+    public AbstractCar getCar() {
+        return car;
+    }
+
+    public void setCar(AbstractCar car) {
+        this.car = car;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+}
